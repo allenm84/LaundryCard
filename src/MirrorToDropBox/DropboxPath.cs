8e7fe578463e22adf59588fdb4a5cff628a275f9
Michael Allen <michael.allen84+git@gmail.com>
2017-11-08 17:32:28 -0600

    Upgrade to devexpress 14.2.15

---
+++ src/MirrorToDropBox/DropboxPath.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace MirrorToDropBox
+{
+  public static class DropboxPath
+  {
+    public static string Value => ReadConfig();
+
+    private static string ReadConfig()
+    {
+      var locations = new[]
+      {
+        Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+        Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+      };
+
+      foreach (var location in locations)
+      {
+        var filepath = Path.Combine(location, @"Dropbox\info.json");
+        if (!File.Exists(filepath))
+        {
+          continue;
+        }
+
+        var value = JObject.Parse(File.ReadAllText(filepath));
+        var path = value["personal"].Value<string>("path");
+        return path;
+      }
+
+      throw new Exception(@"Dropbox\info.json not found.");
+    }
+  }
+}
---
+++ src/MirrorToDropBox/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MirrorToDropBox
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var source = args[0];
+      Console.Out.WriteLine($"\t\tsrc: {source}");
+
+      var destination = Path.Combine(DropboxPath.Value, args[1]);
+      Console.Out.WriteLine($"\t\tdst: {destination}");
+
+      var process = new Process();
+      process.StartInfo.FileName = "robocopy";
+      process.StartInfo.Arguments = $"\"{source}\" \"{destination}\" *.dll *.exe /MIR /E /XO /W:5 /R:10";
+      process.StartInfo.UseShellExecute = false;
+      process.StartInfo.CreateNoWindow = true;
+      process.StartInfo.RedirectStandardError = true;
+      process.StartInfo.RedirectStandardOutput = true;
+      process.EnableRaisingEvents = false;
+      process.OutputDataReceived += (s, e) => Console.Out.WriteLine(e.Data);
+      process.ErrorDataReceived += (s, e) => Console.Error.WriteLine(e.Data);
+      process.Start();
+      process.BeginOutputReadLine();
+      process.BeginErrorReadLine();
+      process.WaitForExit();
+    }
+  }
+}
